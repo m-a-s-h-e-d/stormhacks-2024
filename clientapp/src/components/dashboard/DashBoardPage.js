ba8f4e3f2d00e816d41cb02aee3783c259ebc9fb
Soohyeun <soohyeunp@gmail.com>
2024-05-18 23:34:03 -0700

    Adjust title and change exercises

--- clientapp/src/components/dashboard/DashBoardPage.js
+++ clientapp/src/components/dashboard/DashBoardPage.js
@@ -2,38 +2,42 @@ import React from "react";
 import { Flex, Text } from "@chakra-ui/react";
 import WorkoutCard from "./WorkoutCard";
 import LineChart from "./LineChart";
+import { theme } from "@chakra-ui/theme";
 
 export default React.forwardRef((props, ref) => {
+  const colors = theme.colors;
+
   //hardcoded workouts - need to be changed
   const workoutList = [
     {
       title: "Legs",
       exercises: [
-        "Lorem ipsum dolor sit amet",
-        "Consectetur adipiscing elit",
-        "Integer molestie lorem at massa<",
+        "Squats - 3 sets of 10 reps each",
+        "Lunges - 3 sets of 10 reps per leg",
+        "Deadlifts - 3 sets of 10 reps",
       ],
     },
     {
       title: "Arms",
       exercises: [
-        "Lorem ipsum dolor sit amet",
-        "Consectetur adipiscing elit",
-        "Integer molestie lorem at massa<",
+        "Bicep Curls - 3 sets of 10 reps each",
+        "Tricep Dips - 3 sets of 10 reps",
+        "Push-ups - 3 sets of 10 reps",
       ],
     },
     {
       title: "Back",
       exercises: [
-        "Lorem ipsum dolor sit amet",
-        "Consectetur adipiscing elit",
-        "Integer molestie lorem at massa<",
+        "Pull-ups - 3 sets of 10 reps",
+        "Dumbbell Rows - 3 sets of 10 reps per arm",
+        "Deadlifts - 3 sets of 10 reps",
       ],
     },
   ];
+
   return (
     <Flex direction={"column"} w={"100%"} h={"100%"} gap={"2rem"}>
-      <Text>Welcome to FitFriend!</Text>
+      <Flex justify={"center"} fontSize={"30px"} mt="20px" fontWeight={"bold"} color={colors.teal[800]}>Welcome to FitFriend!</Flex>
       <Flex direction={"row"} w={"100%"} h={"150px"} flex={1} gap={"1rem"}>
         <Flex flex={2} h={"100%"}>
           <LineChart />
