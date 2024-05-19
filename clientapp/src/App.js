f02d8a23b1a00f8eb170e443b18a965ae4acc4fc
Kent-Danielle <kconcengco@my.bcit.ca>
2024-05-18 23:59:47 -0700

    fixed merge conflict

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -3,15 +3,8 @@ import "./App.css";
 import LoginPage from "./components/login/LoginPage";
 import { useAuth } from "./context/AuthContext";
 import WorkoutPage from "./components/workout/WorkoutPage";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import PoseDetection from "./components/pose-detection/PoseDetection";
-=======
-import { Button } from "@chakra-ui/react";
->>>>>>> f703ce33d4d2f5f9f3419ab6eb47182e77ce2beb
-=======
 import DashBoardPage from "./components/dashboard/DashBoardPage";
->>>>>>> 1f0960cf12f6323721386052a203f0d3fcffa9b3
 
 function App() {
 	const { isAuthenticated, isAdmin } = useAuth();
